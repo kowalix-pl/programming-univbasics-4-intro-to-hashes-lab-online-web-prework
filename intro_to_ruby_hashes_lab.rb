0749378e719ca2dcaed1142de522c43f3f377034
kowalix007 <kowalix007@gmail.com>
2019-11-13 13:47:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,9 @@ end
 
 def my_hash_creator(key, value)
 # return a hash that includes the key and value parameters passed into this method
+  my_hash_creator = Hash.new
   my_hash_creator = {:name => "John", :age => "21",:address => "NY"}
+  
  puts 
  end
 

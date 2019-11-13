26e6d4edf340c2bd5cf3da4ebbd80ff32b8f6722
kowalix007 <kowalix007@gmail.com>
2019-11-13 13:21:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,8 @@ end
 
 def my_hash_creator(key, value)
 # return a hash that includes the key and value parameters passed into this method
-  a = {name: "John", age: "21"
+  a = {name: "John", age: "21", address "NY"}
+ a.each
  end
 
 def read_from_hash(hash, key)
